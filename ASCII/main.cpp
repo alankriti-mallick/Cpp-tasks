93ef9bb0d94cd94ef08b7ecfd73a3621479e7724
dreaming-wizard <58414077+dreaming-wizard@users.noreply.github.com>
2020-01-30 20:58:22 +0530

    Add files via upload

---
+++ ASCII/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<< "Enter a character: \n";
+
+    char x;
+    cin>> x;
+
+    cout<< "ASCII value: "<< int (x);
+
+    return 0;
+}
